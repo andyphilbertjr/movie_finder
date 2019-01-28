239a825e22e0e990edc668433b0a1c440f44d134
Andy Philbert Jr <andy@hmtechz.com>
2019-01-28 16:04:08 -0500

    starter code for results movie click event

--- node-backend-starter-code/public/scripts/index.js
+++ node-backend-starter-code/public/scripts/index.js
@@ -2,16 +2,31 @@ const Api_Key = 'cd37cf2f'
 let url = `http://www.omdbapi.com/?apikey=${Api_Key}&`
 const form = document.querySelector('form')
 const results = document.querySelector('.results')
+//
 
+// function dipslayMovieDetails(){
   
+//   document.querySelectorAll('.movies').addEventListener('click', function(e){
+//     console.log(e.target.value.imdbID.value)
+//     e.preventDefault()
+//     fetch(`${url}i=${e.target.imdbID.value}` ).
+//     then(response => response.json()).
+//     then( data => {
+//       movies.insertAdjacentHTML('beforeend'
+//       `<div class='movieDetails'>${data.Plot}<div>`
+//      )
+//     }) 
+//   })
+// }
 
+//wrap function in objects // make calls from the object making it privat
 form.addEventListener('submit' ,function(e){
   e.preventDefault()
+
   results.innerHTML = ''
   fetch(url + 's=' + form.querySelector('input').value)
   .then( response => response.json() )
   .then( data => {
-    console.log(data)
     return data.Search.map( (movie, key) => {
       results.insertAdjacentHTML('afterbegin',
       `
@@ -25,6 +40,4 @@ form.addEventListener('submit' ,function(e){
     })
   })
 
-  form.reset();
-  }); 
-
+  // movies.addEventListener('onload')
