f86c58fab83528c8d5b1050a1af24fe208fbce8b
andyphilbertjr <andyphilbertjr@gmail.com>
2019-01-28 03:03:23 -0500

    results ui fixed

--- node-backend-starter-code/public/scripts/index.js
+++ node-backend-starter-code/public/scripts/index.js
@@ -1,26 +1,30 @@
-
-  const Api_Key = 'cd37cf2f'
-  let url = `http://www.omdbapi.com/?apikey=${Api_Key}&`
-  const form = document.querySelector('form')
-  let results = document.querySelector('.results')
-  const movieContainer = document.createElement('div')
+const Api_Key = 'cd37cf2f'
+let url = `http://www.omdbapi.com/?apikey=${Api_Key}&`
+const form = document.querySelector('form')
+const results = document.querySelector('.results')
 
   
 
-
-  form.addEventListener('submit' ,function(e){
-    e.preventDefault()
-    console.log(url + 's=' + form.querySelector('input').value)
-    fetch(url + 's=' + form.querySelector('input').value)
-    .then( response => response.json() )
-    .then( data => {
-      console.log(data)
-      return data.Search.map( (movie, key) => {
-        let container = results.appendChild(movieContainer)
-        container.insertAdjacentHTML('afterbegin',
-        `<h1>${movie.Title}</h1><img src=${movie.Poster} /><p>${movie.Year}, Imdb ID: ${movie.imdbID}</p>`)
-      })
+form.addEventListener('submit' ,function(e){
+  e.preventDefault()
+  console.log(url + 's=' + form.querySelector('input').value)
+  fetch(url + 's=' + form.querySelector('input').value)
+  .then( response => response.json() )
+  .then( data => {
+    console.log(data)
+    return data.Search.map( (movie, key) => {
+      results.insertAdjacentHTML('afterbegin',
+      `
+        <div class='movies'>
+        <h1>${movie.Title}</h1>
+        <img class='poster' src=${movie.Poster} />
+        <p>${movie.Year}, Imdb ID: ${movie.imdbID}</p>
+        </div>
+      `
+      )
     })
+  })
+
   form.reset();
   }); 
 
