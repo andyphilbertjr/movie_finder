8bb0b20c3cc630b3de086aea73436897a90e79eb
andyphilbertjr <andyphilbertjr@gmail.com>
2019-02-01 17:05:59 -0500

    cleaning up

--- node-backend-starter-code/routes/index.js
+++ node-backend-starter-code/routes/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const fs = require('fs');
 
 // data 
-const db = require('../data.json');
+const db = 'data.json[faves]' ;
 
 
 router.use(express.static(path.join(__dirname, 'views')))
@@ -19,14 +19,15 @@ router.get('/favorites',  getData);
 
 module.exports = router
 
-
-function addToDB(request, response){
-  let data = fs.writeFile('data.json', JSON.stringify(request.body), ()=>   console.log('file added'))
-  response.send(data)
+function addToDB(request, response, next){
+  let movie = JSON.stringify(request.body, null, 2)
+  fs.appendFile('data.json', ',' + movie, () => console.log('movie added'))
+  response.send(movie)
+  next()
 }
 
 function getData(request, response){
-  let data = fs.readFileSync('data.json')
-  let favoriteMovies = JSON.parse(data)
-  response.json(favoriteMovies)
+  let data = fs.readFile('data.json')
+  console.log(data)
+  response.json(data)
 }
